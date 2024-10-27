e4dc035a2b59764f4d906de8e25344f441ed15f4
Thread-ad <agris123@gmail.com>
2024-10-27 16:22:32 +0000

    Set up complete

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_MAX_RADIUS
+from constants import ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE, ASTEROID_KINDS
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
