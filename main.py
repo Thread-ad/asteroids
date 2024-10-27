2a0eaa2cfbdb1f2ca8604ba401dd4ff40b69bb66
Thread-ad <agris123@gmail.com>
2024-10-27 19:56:26 +0000

    Asteroid movement

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_MAX_RADIUS
-from constants import ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE, ASTEROID_KINDS
+from constants import *
+from player import *
+from circleshape import *
+
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    ship = player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
+        ship.update(dt)
+        screen.fill((0,0,0))
+        ship.draw(screen)
+        print(f"dt is: {dt}\nrotation is: {ship.rotation}")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+        
+
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
